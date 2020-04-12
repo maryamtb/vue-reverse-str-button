302ad1e5023180a8c43541e697653c5237c7c2f2
maryamtb <maryamtb.17@gmail.com>
2020-04-12 11:36:38 +0100

    inital commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+import tinylib from './tinylib.vue'
 
-Vue.config.productionTip = false
+function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("tinylib", tinylib);
+}
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const plugin = {
+  install
+};
+
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.vue;
+}
+
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
+
+tinylib.install = install;
+
+export default tinylib;
---
+++ src/tinylib.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">Reverse Message</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "tinylib",
+  data: function() {
+    return {
+      message: {
+        type: String,
+        default: "Hello!"
+      }
+    }
+  },
+  methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  };
+</script>
+
+<style>
+
+</style>
